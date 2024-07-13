37a278e5720bf8ebc16f8d4f1d019c9dbe8700e0
Wangyingying-source <2390459624@qq.com>
2024-07-13 22:42:39 +0800

    Delete code directory

--- code/SEVAC/load_data.py
+++
@@ -1,4 +0,0 @@
-from imports import *
-
-def load_data(file_path):
-    return pd.read_csv(file_path)
--- code/SEVAC/theta_operations.py
+++
@@ -1,3 +0,0 @@
-def update_theta(G, selected_edges_probs, reward, alpha):
-    for u, v, prob in selected_edges_probs:
-        G[u][v]['theta'] += alpha * reward * (1 - prob)
